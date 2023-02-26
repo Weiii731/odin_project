55058284423438aabb23f9c2d5b2a149e34e3172
Weiyang Li <weiyangli94@gmail.com>
2023-02-26 14:12:37 -0500

    Upload calculator project

---
+++ Calculator/scripts.js
@@ -0,0 +1,54 @@
+const numberPad = ["AC", "+/-", "%", "÷", 
+                   "7", "8", "9", "x",
+                   "4", "5", "6", "-", 
+                   "1", "2", "3", "+",
+                   "0", ".", "=" ];
+
+const elementIds = ["clear", "sign", "precent",
+                    "divide", "seven", "eight", "nine",
+                    "multiply", "four", "five", "six", 
+                    "minus", "one", "two", "three",
+                    "plus", "zero", "period", "equal"];
+
+let add = function(a, b) {
+    return a + b;
+}
+
+let subtract = function(a, b) {
+    return a - b;
+}
+
+let multiply = function(a, b) {
+    return a * b;
+}
+
+let divide = function(a, b) {
+    return a / b;
+}
+
+function operate(operation, a, b) {
+    switch(operation) {
+        case "add":
+            return add(a, b);
+        case "subtract":
+            return subtract(a, b);
+        case "multiply":
+            return multiply(a, b);
+        case "divide":
+            return divide(a, b);
+    }
+}
+
+function init() {
+    const container = document.querySelector(".container");
+    let screen = document.createElement("div");
+    screen.setAttribute("id", "screen");
+    container.appendChild(screen);
+    for (let i = 0; i < elementIds.length; i++) {
+        let button = document.createElement("button");
+        button.setAttribute("id", `${elementIds[i]}`);
+        button.innerHTML = `${numberPad[i]}`;
+        container.appendChild(button);
+    };
+}
+init();
