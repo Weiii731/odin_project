7bd1a5bf15c3661fb2ad0fcc097bc203762726f2
Weiyang Li <weiyangli94@gmail.com>
2023-02-26 15:56:58 -0500

    using javascript to generate all buttons on screen

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -4,7 +4,7 @@ const numberPad = ["AC", "+/-", "%", "÷",
                    "1", "2", "3", "+",
                    "0", ".", "=" ];
 
-const elementIds = ["clear", "sign", "precent",
+const elementIds = ["clear", "sign", "percent",
                     "divide", "seven", "eight", "nine",
                     "multiply", "four", "five", "six", 
                     "minus", "one", "two", "three",
@@ -43,12 +43,15 @@ function init() {
     const container = document.querySelector(".container");
     let screen = document.createElement("div");
     screen.setAttribute("id", "screen");
+    screen.setAttribute("style", "grid-area: screen;");
     container.appendChild(screen);
     for (let i = 0; i < elementIds.length; i++) {
         let button = document.createElement("button");
         button.setAttribute("id", `${elementIds[i]}`);
+        button.setAttribute("style", `grid-area: ${elementIds[i]};`);
         button.innerHTML = `${numberPad[i]}`;
         container.appendChild(button);
     };
 }
+
 init();
